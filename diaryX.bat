4e4cea2eee788225a541a647ab26300b152fce81
Bebofireboy <familygames220@outlook.com>
2026-03-06 21:53:23 -0600

    Add files via upload

---
+++ diaryX.bat
@@ -0,0 +1,12 @@
+@echo off
+set a=diaryX.txt
+:home
+cls
+color f0
+title Your own personal diary, private, free, forever! All local on your computer!
+echo Type anything, this is your diary :)
+set /p "input=>"
+echo. >> %a%
+echo %time% on %date% >> %a%
+echo %input% >> %a%
+goto home
